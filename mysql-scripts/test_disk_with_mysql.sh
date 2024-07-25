b5cd4635449e6d4265ed93116211c921f40ed3c0
liuya.diego <oucliuya@163.com>
2024-07-25 21:52:59 +0800

    adjust storage test scripts

--- mysql-scripts/test_disk_with_mysql.sh
+++ mysql-scripts/test_disk_with_mysql.sh
@@ -8,6 +8,7 @@ table_size=10000000
 table_count=16
 run_time=300
 threads=(1 2 4 8)
+device=/dev/vdc
 
 for num_threads in ${threads[@]}; do
   echo "${num_threads} threads read_write ======================="
@@ -18,7 +19,7 @@ for num_threads in ${threads[@]}; do
   --tables=$table_count --time=$run_time --threads=${num_threads} \
   --report-interval=20 oltp_read_write run > ${num_threads}_threads_read_write.log 2>&1 &
   nohup iostat -x -m 5 > ${num_threads}_threads_read_write_iostat.log 2>&1 &
-  nohup  blktrace -d /dev/vdb -o ${num_threads}_threads_read_write_blktrace.log -w 120 > ${num_threads}_threads_read_write_blk2.log 2>&1 &
+  nohup  blktrace -d $device -o ${num_threads}_threads_read_write_blktrace.log -w 120 > ${num_threads}_threads_read_write_blk2.log 2>&1 &
   sleep 305
   pkill -9 iostat
   echo "end test:`date`"
@@ -31,7 +32,7 @@ for num_threads in ${threads[@]}; do
   --tables=$table_count --time=$run_time --threads=${num_threads} \
   --report-interval=20 oltp_write_only run > ${num_threads}_threads_write_only.log 2>&1 &
   nohup iostat -x -m 5 > ${num_threads}_threads_write_only_iostat.log 2>&1 &
-  nohup  blktrace -d /dev/vdb -o ${num_threads}_threads_write_only_blktrace.log -w 120 > ${num_threads}_threads_write_only_blk2.log 2>&1 &
+  nohup  blktrace -d $device -o ${num_threads}_threads_write_only_blktrace.log -w 120 > ${num_threads}_threads_write_only_blk2.log 2>&1 &
   sleep 305
   pkill -9 iostat
   echo "end test:`date`"
@@ -44,7 +45,7 @@ for num_threads in ${threads[@]}; do
   --tables=$table_count --time=$run_time --threads=${num_threads} \
   --report-interval=20 oltp_read_only run > ${num_threads}_threads_read_only.log 2>&1 &
   nohup iostat -x -m 5 > ${num_threads}_threads_read_only_iostat.log 2>&1 &
-  nohup  blktrace -d /dev/vdb -o ${num_threads}_threads_read_only_blktrace.log -w 120 > ${num_threads}_threads_read_only_blk2.log 2>&1 &
+  nohup  blktrace -d $device -o ${num_threads}_threads_read_only_blktrace.log -w 120 > ${num_threads}_threads_read_only_blk2.log 2>&1 &
   sleep 305
   pkill -9 iostat
   echo "end test:`date`"
